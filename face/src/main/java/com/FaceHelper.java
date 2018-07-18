ca31274d47050fad16f62887e6ebb136db5c5037
andyxm <407311996@qq.com>
2018-07-18 15:39:32 +0800

    Merge branch 'master' of https://github.com/andyxm/ArcFace-Demo
    # Conflicts:
    #	README.md

--- face/src/main/java/com/FaceHelper.java
+++ face/src/main/java/com/FaceHelper.java
@@ -2,12 +2,12 @@ package com;
 import com.face.lib.FaceDataHelper;
 import com.model.StudentModel;
 public class FaceHelper extends FaceDataHelper<StudentModel>{
-	public static String appid = "DvyLb4FDPPY8SqZcz67QUxwfF2h2UxXb9i8z7561np3p";
-	public static String ft_key = "6ufZxr4Y7KH9uzLShaXdmFuo2ooj5gaa6Tq5QXrLAp6T";
-	public static String fd_key = "6ufZxr4Y7KH9uzLShaXdmFuvCD4toevs1YWdGF9Tsvns";
-	public static String fr_key = "6ufZxr4Y7KH9uzLShaXdmFvHgQrNnmynxM7mhwbRwrMn";
-	public static String age_key = "6ufZxr4Y7KH9uzLShaXdmFvfAcdufhbxea8jNu5fJPYz";
-	public static String gender_key = "6ufZxr4Y7KH9uzLShaXdmFvnL1u46zVpW5wn7XAuxotS";
+	public static String appid = "xxxxxxxxxxxxxxxxxxx";
+	public static String ft_key = "xxxxxxxxxxxxxxxxxxx";
+	public static String fd_key = "xxxxxxxxxxxxxxxxxxx";
+	public static String fr_key = "xxxxxxxxxxxxxxxxxxx";
+	public static String age_key = "xxxxxxxxxxxxxxxxxxx";
+	public static String gender_key = "xxxxxxxxxxxxxxxxxxx";
 	public FaceHelper(String path) {
 		super(path);
 	}
